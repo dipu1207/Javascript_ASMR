d4c80ad84d8cf766e695e5ca3c0a8aff8dcda412
dipu1207 <dipu.nauni12071999@gmail.com>
2023-08-01 21:06:33 +0530

    added first project

---
+++ 1_Aspect_Ratio_Calculator/script.js
@@ -0,0 +1,22 @@
+let ratioWidth=document.getElementById("ratio-width");
+let ratioHeight=document.getElementById("ratio-height");
+let width=document.getElementById("width");
+let height=document.getElementById("height");
+
+let calculateWidth = () =>{
+    let aspectRatio=ratioWidth.value / ratioHeight.value;
+    width.value=parseFloat((height.value*aspectRatio).toFixed(2));
+    return width.value;
+};
+
+
+let calculateHeight = () =>{
+    let aspectRatio=ratioWidth.value / ratioHeight.value;
+    height.value=parseFloat((width.value*aspectRatio).toFixed(2));
+    return height.value;
+};
+
+height.addEventListener("input", calculateWidth);
+width.addEventListener("input", calculateHeight);
+ratioHeight.addEventListener("input", calculateWidth);
+ratioWidth.addEventListener("input", calculateHeight);
